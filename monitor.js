461fd1643bf2e6fd789bd651b2578558734362f9
Jens Jaeger <jaegerj204@googlemail.com>
2017-03-04 12:44:52 +0100

    run endless with interval and better duration output

--- monitor.js
+++ monitor.js
@@ -1,27 +1,37 @@
 const fetch = require('node-fetch');
-
+const milli = 1000;
+const nano = milli * milli * milli;
 const config = {
     requests: [
         'http://192.168.178.1/css/rd/images/fritzLogo.svg',
-        'https://i-ssl.ligatus.com/blank.gif'
+        'https://www.heise.de/avw-bin/ivw/CP/marston/'
     ],
-    interval: 10000
+    interval: 3 * milli // milliseconds
 };
 
-for(let endpoint of config.requests) {
-    timeRequest(endpoint);
+setInterval(run, config.interval);
+
+function run() {
+    for(let endpoint of config.requests) {
+        timeRequest(endpoint);
+    }
 }
 
 function timeRequest(url) {
     console.log(timestamp() + 'start request ' + url);
     let start = process.hrtime();
     fetch(url).then(res => {
-        let duration = process.hrtime(start);
-        console.log(timestamp() + 'duration of request ' + url +' ### ' + duration);
+        let duration = toMilliseconds(process.hrtime(start));
+        console.log(timestamp() + 'duration of request ' + url +' ### ' + duration + ' ms');
     })
 }
 
 function timestamp() {
     let now = new Date();
     return '[' + now.toISOString() + '] ';
+}
+
+function toMilliseconds(hrtime) {
+    let ms = hrtime[0] * milli + hrtime[1] * milli / nano; // duration in milliseconds
+    return ms;
 }
