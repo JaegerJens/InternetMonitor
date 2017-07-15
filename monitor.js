fcb51da2d7f164bb5d99bf6b15a5cdc47a3ebeba
Jens Jaeger <jaegerj204@googlemail.com>
2017-07-15 13:38:12 +0200

    refactor: move logging into module

--- monitor.js
+++ monitor.js
@@ -1,29 +1,12 @@
 const fetch = require('node-fetch');
 const colors = require('colors');
-const notifier = require('node-notifier');
 const fs = require('fs');
-const os = require('os');
 const util = require('util');
+const log = require('./lib/log');
 
 const readFileAsync = util.promisify(fs.readFile);
 
 const configFile = 'monitor.config.json';
-/** @type {Number} */
-const milli = 1000;
-const nano = milli * milli * milli;
-
-/** @type {WriteStream} */
-let outputStream = undefined;
-
-/** @type {boolean} */
-let isOffline = false;
-
-async function main() {
-    const config = await readConfig();
-    outputStream = createOutputStream(config.output);
-    setInterval(run, config.interval, config);
-}
-main();
 
 /**
  * @param {Object} config 
@@ -39,20 +22,20 @@ function run(config) {
  * @returns {Promise} 
  */
 async function timeRequest(url) {
-    await logEvent('start request '.grey + url.blue);
+    await log.logEvent('start request '.grey + url.blue);
     let start = process.hrtime();
     try {
         const res = await fetch(url);
         if (!res.ok) {
-            await logError('HTTP', res.statusText + ' for ' + url.blue);
+            await log.logError('HTTP', res.statusText + ' for ' + url.blue);
         } else {
-            let duration = toMilliseconds(process.hrtime(start));
+            let duration = log.toMilliseconds(process.hrtime(start));
             let col = evaluateDuration(duration);
-            await logEvent(col('duration of request ') + url.blue + col(' ### ' + duration + ' ms'));
+            await log.logEvent(col('duration of request ') + url.blue + col(' ### ' + duration + ' ms'));
         }
     }
     catch (ex) {
-        await logError('FETCH', ex);
+        await log.logError('FETCH', ex);
     }
 }
 
@@ -72,77 +55,17 @@ function evaluateDuration(timespan) {
 }
 
 /**
- * @returns {string}
- */
-function timestamp() {
-    let now = new Date();
-    return '[' + now.toISOString() + '] ';
-}
-
-/**
- * @param {Number} hrtime
- * @returns {Number}
- */
-function toMilliseconds(hrtime) {
-    let ms = hrtime[0] * milli + hrtime[1] * milli / nano; // duration in milliseconds
-    return ms;
-}
-
-async function logError(category, errormessage) {
-    if (!isOffline) {
-        notifier.notify('Internet is Offline!');
-        isOffline = true;
-    }
-    await logEvent(`${category} ERROR: ${errormessage}`.red);
-}
-
-/**
- * @param {string} event
- * @returns {Promise}
- */
-function logEvent(event) {
-    let text = colors.magenta(timestamp()) + event;
-    console.log(text);
-    const eol = os.EOL;
-    let cleanText = colors.stripColors(text);
-    return outputStream.WriteAsync(cleanText + eol, null);
-}
-
-/**
- * @param {string} filename
- * @returns {WriteStream}
- */
-function createOutputStream(filename) {
-    const logFile = replaceDate(filename);
-    console.log(`Logfile output: ${logFile}`.grey);
-    let output = fs.createWriteStream(logFile, {encoding: 'utf8', flags: 'a'});
-    output.WriteAsync = util.promisify(output.write);
-    return output;
-}
-
-/**
- * @param {string} filename
- * @returns {string}
- */
-function replaceDate(filename) {
-    const variable = "{date}";
-    let now = getNowDate();
-    return filename.replace(variable, now);
-}
-
-/**
- * @returns {string}
- */
-function getNowDate() {
-    let now = new Date();
-    let iso = now.toISOString().slice(0,10).replace(/-/g,'');
-    return iso;
-}
-
-/**
  * @returns {Promise<Object>}
  */
 async function readConfig() {
     const data = await readFileAsync(configFile, 'utf8');
     return JSON.parse(data);
 }
+
+async function main() {
+    const config = await readConfig();
+    log.openLogFile(config.output);
+    setInterval(run, config.interval, config);
+}
+
+main();
